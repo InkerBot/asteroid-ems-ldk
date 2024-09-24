e58d3179fa04bdfebbc33802086a4590ccc42d03
InkerBot <im@inker.bot>
2024-09-25 03:42:37 +0800

    publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "io.asteroid.ldk"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -13,4 +13,20 @@ repositories {
 java {
     withJavadocJar()
     withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven("https://s0.blobs.inksnow.org/maven/") {
+            credentials {
+                username = System.getenv("IREPO_USERNAME")
+                password = System.getenv("IREPO_PASSWORD")
+            }
+        }
+    }
 }
